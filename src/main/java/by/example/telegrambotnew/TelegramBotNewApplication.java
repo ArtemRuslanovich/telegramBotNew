9b1737699f4d68ff5a1128924372bc85814da7d7
Artiom Zavatski <zawatski2018@mail.ru>
2023-05-19 14:38:31 +0300

    Create new bot

---
+++ src/main/java/by/example/telegrambotnew/TelegramBotNewApplication.java
@@ -0,0 +1,13 @@
+package by.example.telegrambotnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/example/telegrambotnew/TelegramBotNewApplicationTests.java
@@ -0,0 +1,13 @@
+package by.example.telegrambotnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
